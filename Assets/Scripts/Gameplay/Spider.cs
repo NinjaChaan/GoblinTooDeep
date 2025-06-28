3e5e94eb8f145d9f967e16bb5ca13197af5d8d10
Nominom <7089998+Nominom@users.noreply.github.com>
2025-06-28 18:57:29 +0300

    spiders

--- Assets/Scripts/Gameplay/Spider.cs
+++ Assets/Scripts/Gameplay/Spider.cs
@@ -6,13 +6,24 @@ using Random = UnityEngine.Random;
 
 public class Spider : MonoBehaviour
 {
+    public enum SpiderState
+    {
+        Idle,
+        ChasingGem,
+        CarryingGem,
+        FleeingTorch
+    }
+    
     private GameObject targetGem;
     public float speed = 0.1f;
     public float speedWithGem = 0.1f;
+    public float wanderDistance = 3f;
+    public float gemPickupRange = 0.3f;
 
     // private Rigidbody rb;
     public Transform gemHoldPoint;
 
+    public GameObject holdingGem;
     private bool isHoldingGem = false;
     public Transform spiderHole; // Reference to the spider hole where the gem will be dropped
 
@@ -24,78 +35,198 @@ public class Spider : MonoBehaviour
 
     private NavMeshAgent navMeshAgent; // Reference to the NavMeshAgent component for pathfinding
     private NavMeshPath path;
+    
+    public SpiderState CurrentState = SpiderState.Idle;
 
+
+    private float idleTimer = 0f;
+    private float fleeTimer = 5f;
+    
     void Start()
     {
+        var holes = FindObjectsByType<SpiderHole>(FindObjectsSortMode.None);
+        
+        spiderHole = holes[Random.Range(0, holes.Length)].transform; // Find hole
+        
         // rb = GetComponent<Rigidbody>();
         navMeshAgent = GetComponent<NavMeshAgent>();
         // Start checking for gems in the scene
-        StartCoroutine(CheckForGems());
         StartCoroutine(CheckForTorches());
+        
+        CurrentState = SpiderState.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        avoidingTorch = false; // Reset the avoidingTorch flag at the start of each update
-        if (torches.Length > 0)
+        if (!navMeshAgent.isOnNavMesh)
+        {
+            if (!NavMesh.FindClosestEdge(transform.position, out var hit, NavMesh.AllAreas))
+            {
+                Debug.LogError("No NavMesh found!");
+                return;
+            }
+            navMeshAgent.Warp(hit.position);
+        }
+        
+        if (torches.Length > 0 && CurrentState != SpiderState.FleeingTorch)
         {
             foreach (var torch in torches)
             {
                 if (Vector3.Distance(torch.transform.position, transform.position) < torchEscapeDistance)
                 {
-                    avoidingTorch = true; // Set the flag to indicate the spider is avoiding a torch
-                    // If the spider is too close to a torch, escape in a random direction
-                    Vector3 escapeDirection = (transform.position - torch.transform.position).normalized;
-                    //rb.MovePosition(transform.position + escapeDirection * (speed * Time.deltaTime));
-                    // Rotate away from the torch
-                    Quaternion lookRotation = Quaternion.LookRotation(escapeDirection);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+                    StartFlee(torch.transform);
                     break;
                 }
             }
         }
-
-        if (targetGem && !isHoldingGem)
+        
+        if (isHoldingGem)
         {
-            navMeshAgent.destination = targetGem.transform.position;
-            // Move towards the target gem
-            Vector3 direction = (targetGem.transform.position - transform.position).normalized;
-            // rb.MovePosition(transform.position + direction * (speed * Time.deltaTime));
-            //navMeshAgent.Move(direction * (speed * Time.deltaTime));
-            // Rotate towards the target gem
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            navMeshAgent.speed = speedWithGem;
+        } else
+        {
+            navMeshAgent.speed = speed;
+        }
+
+        if (CurrentState == SpiderState.Idle)
+        {
+            Idle();
+        }else if (CurrentState == SpiderState.ChasingGem)
+        {
+            ChasingGem();
+        } else if (CurrentState == SpiderState.CarryingGem)
+        {
+            CarryingGem();
+        } else if (CurrentState == SpiderState.FleeingTorch)
+        {
+            FleeingTorch();
+        }
+    }
+
+    private void Idle()
+    {
+        if (idleTimer > 0f)
+        {
+            idleTimer -= Time.deltaTime;
+            return;
         }
 
         if (isHoldingGem)
         {
-            navMeshAgent.destination = spiderHole.position;
-            //Move towards spiderhole
-            Vector3 direction = (spiderHole.position - transform.position).normalized;
-            // navMeshAgent.Move(direction * (speed * Time.deltaTime));
-            // rb.MovePosition(transform.position + direction * (speedWithGem * Time.deltaTime));
-            // Rotate towards the spider hole
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            CurrentState = SpiderState.CarryingGem;
+            return;
+        }
+        
+        if (Random.Range(0, 1f) < 0.3f)
+        {
+            FindTargetGem();
+        }
+        else
+        {
+            navMeshAgent.destination = GetRandomNearTargetPosition();
+            idleTimer = Random.Range(1f, 5f);
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void ChasingGem()
     {
-        if (other.transform.CompareTag("Gem"))
+        if (!targetGem)
         {
-            if (isHoldingGem)
-            {
-                return; // If already holding a gem, do nothing
-            }
-            isHoldingGem = true;
-            targetGem.transform.parent = transform;
-            // targetGem.GetComponent<Rigidbody>().isKinematic =
-            //     true; // Make the gem kinematic to stop physics interactions
-            targetGem.GetComponent<Collider>().enabled = false; // Disable collider for the gem
-            targetGem.transform.position = gemHoldPoint.position;
+            CurrentState = SpiderState.Idle;
+            return;
         }
+        if (HasPathEnded())
+        {
+            navMeshAgent.destination = targetGem.transform.position;
+        }
+
+        if (Vector3.Distance(transform.position, targetGem.transform.position) < gemPickupRange)
+        {
+            GrabGem(targetGem);
+        }
+    }
+    
+    private void CarryingGem()
+    {
+        if (!holdingGem || !isHoldingGem)
+        {
+            CurrentState = SpiderState.Idle;
+            return;
+        }
+        if (HasPathEnded())
+        {
+            navMeshAgent.destination = spiderHole.position;
+        }
+        
+        if (Vector3.Distance(transform.position, spiderHole.position) < gemPickupRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void FleeingTorch()
+    {
+        if (HasPathEnded())
+        {
+            CurrentState = SpiderState.Idle;
+            idleTimer = 5f;
+        }
+    }
+
+    private void StartFlee(Transform torch)
+    {
+        Vector3 fleeDirection = (transform.position - torch.position).normalized;
+        navMeshAgent.SetDestination(transform.position + fleeDirection * 10f); // Move away from the torch
+        
+        CurrentState = SpiderState.FleeingTorch;
+        
+        // Rotate away from the torch
+        // Quaternion lookRotation = Quaternion.LookRotation(fleeDirection);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+        if (isHoldingGem)
+        {
+            DropGem();
+        }
+    }
+
+    private void GrabGem(GameObject gem)
+    {
+        if (isHoldingGem)
+        {
+            return; // If already holding a gem, do nothing
+        }
+        isHoldingGem = true;
+        targetGem.transform.parent = gemHoldPoint;
+        targetGem.GetComponent<Rigidbody>().isKinematic = true;
+        targetGem.GetComponent<Collider>().enabled = false;
+        targetGem.transform.position = gemHoldPoint.position;
+        holdingGem = gem;
+
+        CurrentState = SpiderState.CarryingGem;
+    }
+
+    private void DropGem()
+    {
+        holdingGem.transform.parent = null;
+        holdingGem.GetComponent<Rigidbody>().isKinematic = false;
+        holdingGem.GetComponent<Collider>().enabled = true;
+        
+        isHoldingGem = false;
+        holdingGem = null;
+    }
+
+    private Vector3 GetRandomNearTargetPosition()
+    {
+        Vector2 dir = Random.insideUnitCircle* wanderDistance;
+        Vector3 randomPos = transform.position + new Vector3(dir.x, 0, dir.y);
+        return randomPos;
+    }
+
+    private bool HasPathEnded()
+    {
+        return !navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance;
     }
 
     private IEnumerator CheckForTorches()
@@ -108,29 +239,15 @@ public class Spider : MonoBehaviour
         }
     }
 
-    private IEnumerator CheckForGems()
+    private void FindTargetGem()
     {
-        while (true)
+        if (!targetGem)
         {
-            yield return new WaitForSeconds(1f);
-            if (!targetGem)
+            GameObject[] gems = GameObject.FindGameObjectsWithTag("Gem");
+            if (gems.Length > 0)
             {
-                GameObject[] gems = GameObject.FindGameObjectsWithTag("Gem");
-                if (gems.Length > 0)
-                {
-                    targetGem = gems[Random.Range(0, gems.Length)];
-                }
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.transform.CompareTag("SpiderHole"))
-        {
-            if (isHoldingGem)
-            {
-                Destroy(gameObject); // Destroy the spider when it reaches the hole}
+                targetGem = gems[Random.Range(0, gems.Length)];
+                CurrentState = SpiderState.ChasingGem;
             }
         }
     }
--- Assets/Scripts/Gameplay/SpiderHole.cs
+++ Assets/Scripts/Gameplay/SpiderHole.cs
@@ -1,20 +1,34 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpiderHole : MonoBehaviour
 {
-    public GameObject spiderPrefab; 
+    public GameObject spiderPrefab;
+    public float initialSpawnDelayMin = 5f;
+    public float initialSpawnDelayMax = 30f;
+    public float spawningDelay = 30f;
+
+    public int maxSpidersSpawned = 10;
+    
+    private int _currentSpiders = 0;
     
     void Start()
     {
+        float initialDelay = Random.Range(initialSpawnDelayMin, initialSpawnDelayMax);
+        InvokeRepeating(nameof(SpawnSpider), initialDelay, spawningDelay);
+    }
+    
+    private void SpawnSpider()
+    {
+        if (_currentSpiders >= maxSpidersSpawned)
+        {
+            return;
+        }
+        _currentSpiders++;
+        
         var spider = Instantiate(spiderPrefab, transform.position, Quaternion.identity);
         spider.GetComponent<Spider>().spiderHole = transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
