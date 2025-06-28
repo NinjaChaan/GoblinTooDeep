db1d98b6d7e5a4c437bdfd12c9b82702bee195cd
Ninja <sleepyneko@protonmail.ch>
2025-06-28 15:18:11 +0300

    Spoider started. Torch change

---
+++ Assets/Scripts/Gameplay/Spider.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spider : MonoBehaviour
+{
+    private GameObject targetGem;
+    public float speed = 0.1f;
+
+    private Rigidbody rb;
+    public Transform gemHoldPoint;
+    
+    private bool isHoldingGem = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Start checking for gems in the scene
+        StartCoroutine(CheckForGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetGem && !isHoldingGem)
+        {
+            // Move towards the target gem
+            Vector3 direction = (targetGem.transform.position - transform.position).normalized;
+            rb.MovePosition(transform.position + direction * (speed * Time.deltaTime));
+            // Rotate towards the target gem
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+        }
+        // if (isHoldingGem)
+        // {
+        //     // If holding a gem, move it to the hold point
+        //     targetGem.transform.position = gemHoldPoint.position;
+        // }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.CompareTag("Gem"))
+        {
+            isHoldingGem = true;
+            targetGem.transform.parent = transform;
+            targetGem.GetComponent<Rigidbody>().isKinematic = true; // Make the gem kinematic to stop physics interactions
+            targetGem.GetComponent<Collider>().enabled = false; // Disable collider for the gem
+            targetGem.transform.position = gemHoldPoint.position;
+        }
+    }
+
+    private IEnumerator CheckForGems()
+    {
+        while (true)
+        {
+            // Wait for 1 second before checking for gems again
+            yield return new WaitForSeconds(1f);
+            if (!targetGem)
+            {
+                GameObject[] gems = GameObject.FindGameObjectsWithTag("Gem");
+                if (gems.Length > 0)
+                {
+                    targetGem = gems[Random.Range(0, gems.Length)];
+                }
+            }
+        }
+    }
+}
