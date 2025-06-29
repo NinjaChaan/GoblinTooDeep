0d34f1bda686585055b0cc6acd7fbe20fdbd3364
Ninja <sleepyneko@protonmail.ch>
2025-06-29 11:45:01 +0300

    HRT counter

---
+++ Assets/Scripts/UI Scripts/GemAmountScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GemAmountScript : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI gemAmountText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void UpdateGemAmount(int amount)
+    {
+        gemAmountText.text = amount.ToString();
+    }
+}
